1613534a4e0008efe4bb0f919a903294c118ebce
XiXiLocked <xijunyi1990@gmail.com>
2019-02-20 03:00:33 +0800

    test for sst

--- main.py
+++ main.py
@@ -11,15 +11,18 @@ from model import TextCNN
 from data import TextDataset
 import argparse
 
+import torchtext
+from torchtext.data import Field
+
 torch.manual_seed(1)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--lr', type=float, default=0.1)
 parser.add_argument('--batch_size', type=int, default=16)
-parser.add_argument('--epoch', type=int, default=20)
+parser.add_argument('--epoch', type=int, default=2)
 parser.add_argument('--gpu', type=int, default=0)
-parser.add_argument('--out_channel', type=int, default=2)
-parser.add_argument('--label_num', type=int, default=2)
+parser.add_argument('--out_channel', type=int, default=100)
+parser.add_argument('--label_num', type=int, default=3)
 parser.add_argument('--seed', type=int, default=1)
 args = parser.parse_args()
 
@@ -32,56 +35,118 @@ if torch.cuda.is_available():
 # Create the configuration
 config = Config(sentence_max_size=50,
                 batch_size=args.batch_size,
-                word_num=11000,
-                label_num=args.label_num,
+                word_num=25000,
+                label_num= args.label_num,
                 learning_rate=args.lr,
                 cuda=args.gpu,
                 epoch=args.epoch,
                 out_channel=args.out_channel)
 
-training_set = TextDataset(path='data/train')
+             
 
-training_iter = data.DataLoader(dataset=training_set,
-                                batch_size=config.batch_size,
-                                num_workers=2)
+# load SST dataset
+def sst(text_field, label_field,  **kargs):
+    train_data, dev_data, test_data = torchtext.datasets.SST.splits(text_field, label_field)#, fine_grained=True)
+    text_field.build_vocab(train_data, dev_data, test_data)
+    label_field.build_vocab(train_data, dev_data, test_data)
+    train_iter, dev_iter, test_iter = torchtext.data.BucketIterator.splits(
+                                        (train_data, dev_data, test_data), 
+                                        batch_sizes=(args.batch_size, 
+                                                     len(dev_data), 
+                                                     len(test_data)),
+                                        **kargs)
 
+    return train_iter, dev_iter, test_iter 
+             
+text_field = torchtext.data.Field(lower=True)
+label_field = torchtext.data.Field(sequential=False)
+training_iter, valid_iter, test_iter= sst(text_field, label_field,device=-1, repeat=False)
+#training_set = TextDataset(path='data/train',)
+
+#training_iter = data.DataLoader(dataset=training_set,
+#                                batch_size=config.batch_size)
 
 model = TextCNN(config)
-embeds = nn.Embedding(config.word_num, config.word_embedding_dimension)
 
 if torch.cuda.is_available():
     model.cuda()
     embeds = embeds.cuda()
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=config.lr)
+optimizer = optim.SGD(model.parameters(), lr=config.lr,momentum=0.9)
 
 count = 0
 loss_sum = 0
+ 
 # Train the model
 for epoch in range(config.epoch):
-    for data, label in training_iter:
+    for batch in training_iter:
+        data = batch.text;
+        label = batch.label;
+        data.data.t_(), label.data.sub_(1)
+
         if config.cuda and torch.cuda.is_available():
             data = data.cuda()
-            labels = label.cuda()
-
-        input_data = embeds(autograd.Variable(data))
-        out = model(data)
-        loss = criterion(out, autograd.Variable(label.float()))
-
-        loss_sum += loss.data[0]
-        count += 1
-
-        if count % 100 == 0:
-            print("epoch", epoch, end='  ')
-            print("The loss is: %.5f" % (loss_sum/100)）
-
-            loss_sum = 0
-            count = 0
+            label = label.cuda()
+        #input_data = embeds(autograd.Variable(data))
+        input_data = autograd.Variable(data)
+        out = model(input_data.unsqueeze(1))
+        loss = criterion(out, autograd.Variable(label))
 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+        with torch.no_grad():
+            norm = model.linear1.weight.norm()
+            if norm>3 :
+              model.linear1.weight =torch.nn.Parameter((3.0/norm)* model.linear1.weight)
+            loss_sum += loss
+            count += 1
+    
+            if count % 100 == 0:
+                print("epoch", epoch, end='  ')
+                print("The loss is: %.5f" % (loss_sum/100))
+    
+                loss_sum = 0
+                count = 0
+                corrects = (torch.max(out, 1)[1].view(label.size()).data == label.data).sum()
+                accuracy = float(corrects)/config.batch_size * 100.0
+                print("accuracy=%f, "%( accuracy))
+        
+       
     # save the model in every epoch
-    model.save('checkpoints/epoch{}.ckpt'.format(epoch))
+    #model.save('checkpoints/epoch{}.ckpt'.format(epoch))
+    
+    
+confusionMatrix =[]
+for i in range(config.label_num):
+  confusionMatrix.append([])
+  for j in range(config.label_num):
+    confusionMatrix[i].append(0)
 
+loss_total = 0
+count_valid = 0
+for batch  in valid_iter:
+    data = batch.text;
+    label = batch.label;
+    data.data.t_(), label.data.sub_(1)
+    if config.cuda and torch.cuda.is_available():
+            data = data.cuda()
+            label = label.cuda()
+    with torch.no_grad():
+        #input_data = embeds(autograd.Variable(data))
+        input_data = autograd.Variable(data)
+        out = model(input_data.unsqueeze(1))
+        _,pred = torch.max(out,1)
+        loss = criterion(out, autograd.Variable(label))
+        loss_total +=loss
+        count_valid+=1
+        for p,l in zip(list(pred),list(label)):
+            confusionMatrix[p][l]+=1
+
+import pprint            
+pprint.pprint(confusionMatrix)
+print("acc :",
+        sum([confusionMatrix[i][i] for i in range(config.label_num)] )/
+        sum([sum(confusionMatrix[i]) for i in range(config.label_num)]))
+print("loss:",loss_total/count_valid)
--- model/TextCNN.py
+++ model/TextCNN.py
@@ -12,29 +12,52 @@ class TextCNN(BasicModule):
         super(TextCNN, self).__init__()
         self.config = config
         self.out_channel = config.out_channel
-        self.conv3 = nn.Conv2d(1, 1, (3, config.word_embedding_dimension))
-        self.conv4 = nn.Conv2d(1, 1, (4, config.word_embedding_dimension))
-        self.conv5 = nn.Conv2d(1, 1, (5, config.word_embedding_dimension))
-        self.Max3_pool = nn.MaxPool2d((self.config.sentence_max_size-3+1, 1))
-        self.Max4_pool = nn.MaxPool2d((self.config.sentence_max_size-4+1, 1))
-        self.Max5_pool = nn.MaxPool2d((self.config.sentence_max_size-5+1, 1))
-        self.linear1 = nn.Linear(3, config.label_num)
+        
+        D_embed = config.word_embedding_dimension
+        CH_out = config.out_channel
+        size_sentence = config.sentence_max_size
+        self.embedding = nn.Embedding(config.word_num, D_embed)
+
+        #self.conv2 = nn.Conv2d(1, 1, (2, config.word_embedding_dimension))
+        self.conv3 = nn.Conv2d(1, CH_out, (3, D_embed))
+        self.conv4 = nn.Conv2d(1, CH_out, (4, D_embed))
+        self.conv5 = nn.Conv2d(1, CH_out, (5, D_embed))
+        #self.conv6 = nn.Conv2d(1, 1, (6, config.word_embedding_dimension))
+        #self.Max2_pool = nn.MaxPool2d((self.config.sentence_max_size-2+1, 1))
+        self.Max3_pool = nn.MaxPool2d((size_sentence-3+1, 1))
+        self.Max4_pool = nn.MaxPool2d((size_sentence-4+1, 1))
+        self.Max5_pool = nn.MaxPool2d((size_sentence-5+1, 1))
+        #self.Max6_pool = nn.MaxPool2d((self.config.sentence_max_size-6+1, 1))
+        self.dropout = nn.Dropout(0.5)
+        self.linear1 = nn.Linear(3*CH_out, config.label_num)
 
     def forward(self, x):
         batch = x.shape[0]
+        x = self.embedding(x)
         # Convolution
-        x1 = F.relu(self.conv3(x))
-        x2 = F.relu(self.conv4(x))
-        x3 = F.relu(self.conv5(x))
+        #x2 = F.relu(self.conv2(x))
 
+        x3 = F.relu(self.conv3(x))
+        x4 = F.relu(self.conv4(x))
+        x5 = F.relu(self.conv5(x))
+        #x6 = F.relu(self.conv6(x))
+        
         # Pooling
-        x1 = self.Max3_pool(x1)
-        x2 = self.Max4_pool(x2)
-        x3 = self.Max5_pool(x3)
+        #x2 = self.Max2_pool(x2)
+        x3 = self.Max3_pool(x3)
+        x4 = self.Max4_pool(x4)
+        x5 = self.Max5_pool(x5)
+
+        #x6 = self.Max6_pool(x6)
 
         # capture and concatenate the features
-        x = torch.cat((x1, x2, x3), -1)
-        x = x.view(batch, 1, -1)
+        #x = torch.cat((x2,x3,x4,x5,x6), -1)
+        x = torch.cat((
+        x3.view(-1,self.out_channel),
+        x4.view(-1,self.out_channel),
+        x5.view(-1,self.out_channel)), 1)
+        #x = x.view(batch, 1, -1)
+        x = self.dropout(x)
 
         # project the features to the labels
         x = self.linear1(x)
